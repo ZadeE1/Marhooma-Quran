6744a55247f89334e8010e4a15fe176ecb27579b
Zade <88864436+ZadeE1@users.noreply.github.com>
2025-07-10 19:03:27 +1000

    Add Rive plugin support and update related configurations
    - Register Rive plugin in generated_plugin_registrant.cc for Linux and Windows.
    - Include rive_common in plugin lists for both Linux and Windows.
    - Update pubspec.yaml to add rive dependency.
    - Add missing day_night_switch.riv animation asset.
    - Update pubspec.lock to reflect new dependencies.

--- quran_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ quran_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import audio_session
 import just_audio
 import package_info_plus
 import path_provider_foundation
+import rive_common
 import sqflite_darwin
 import wakelock_plus
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
--- quran_app/windows/flutter/generated_plugin_registrant.cc
+++ quran_app/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <just_audio_windows/just_audio_windows_plugin.h>
+#include <rive_common/rive_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   JustAudioWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("JustAudioWindowsPlugin"));
+  RivePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RivePlugin"));
 }
